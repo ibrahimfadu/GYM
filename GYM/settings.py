1d0e09e09f537f0870b0d5db64456b0f2106f1d4
ibrahimfadu <ik09012006@gmail.com>
2025-01-02 01:21:22 -0500

    file

--- GYM/settings.py
+++ GYM/settings.py
@@ -122,3 +122,10 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    'gym-1-ibv7.onrender.com',
+]
